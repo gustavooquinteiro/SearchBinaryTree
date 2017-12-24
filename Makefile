62979779ad4825cb35a62f47ab7c444048b92b05
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2017-12-24 09:54:18 -0300

    Desenvolvimento das funções de inserir nó, atualizar altura e balanceamento

--- Makefile
+++ Makefile
@@ -1,6 +1,12 @@
-#
-all: main.o avl.o
-	gcc -o main.o avl.o -c AgenciaBancariaOtimizada
+# Makefile do Trabalho de Estrutura de Dados e Algoritmos I - MATA40
+# Simulação de Agência Bancária Otimizada 
+# Maiores detalhes do desenvolvimento, vide https://github.com/gustavooquinteiro/SearchBinaryTree
+
+all: avl.o main.o
+	gcc -g avl.o main.o -o AgenciaBancariaOtimizada -Wall -Wextra -Werror -Wpedantic -lm
+
 avl.o:
-	gcc -g avl.c
+	gcc -g -c avl.c -Wall -Wextra -Werror -Wpedantic -lm
+	
 main.o:
+	gcc -g -c main.c -Wall -Wextra -Werror -Wpedantic -lm
--- avl.c
+++ avl.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
-#include <stdio.h>
+#include "avl.h"
 
 struct Cliente{
 	int codigoCliente;
 	int operacao;
 	int valor;
+	int saldo;
 };
 
 typedef struct Arvore {
@@ -27,6 +28,7 @@ No criarNo(int codigoCliente, int operacao, int valor){
 	novoCliente.codigoCliente = codigoCliente;
 	novoCliente.operacao = operacao;
 	novoCliente.valor = valor;
+	novoCliente.saldo = 0;
 	No novoNo = (No *)malloc(1 * sizeof(No);
 	novoNo.cliente = novoCliente;
 	novoNo.direita = NULL;
@@ -36,27 +38,52 @@ No criarNo(int codigoCliente, int operacao, int valor){
 	return novoNo;
 }
 
-void inserirNo(No & arvore, No noAtual){
-	if (arvoreVazia()){
-		arvore.raiz = noAtual;
-	} else{
-		balanceamento(No & arvore);
+No inserirNo(No & arvore, No noAtual){
+	if (arvore == NULL){
+		return noAtual;
+	}else{
 		if (noAtual.cliente.codigoCliente > arvore.cliente.codigoCliente)
-			inserirNo(arvore.direita, noAtual);
+			noAtual.direita = inserirNo(arvore.direita, noAtual);
+			
 		if (noAtual.cliente.codigoCliente == arvore.cliente.codigoCliente)
-			atualizarCliente(noAtual, arvore)
+			noAtual = atualizarCliente(noAtual, arvore)
+			
 		if (noAtual.cliente.codigoCliente < arvore.cliente.codigoCliente)
-			inserirNo(arvore.esquerda, noAtual);
+			noAtual.esquerda = inserirNo(arvore.esquerda, noAtual);
+			
+		noAtual.altura = maximo(getAltura(noAtual.esquerda), getAltura(noAtual.direita)) + 1;
+		if (abs(getAltura(noAtual.direita) - getAltura(noAtual.esquerda)) == 2)
+			balanceamento(noAtual);
+			
+		return noAtual;
 	}
 }
 
-int balanceamento(No * arvore){
-	int coeficienteDeBalanceamento = getAltura(arvore.direita) - getAltura(arvore.esquerda); 
-	if(abs(coeficienteDeBalanceamento) == 1){
-		
-	}else{
-		
+No atualizarCliente(No noAtual, No arvore){
+	if (noAtual.cliente.operacao == 0)
+		arvore.cliente.saldo += noAtual.cliente.valor;
+	else
+		arvore.cliente.saldo -= noAtual.cliente.valor;
+	return arvore;
+}
+
+No balanceamento(No x){
+	if(getAltura(x.direita) - getAltura(x.esquerda) == -2){
+		No y = x.esquerda;
+		if(getAltura(y.direita) - getAltura(y.esquerda) == -1){
+			// Rotação dupla a esquerda
+		} else{
+			// Rotação a esquerda
+		}
+	}else if (getAltura(x.direita) - getAltura(x.esquerda) == 2){
+		No y = x.direita;
+		if (getAltura(y.direita) - getAltura(y.esquerda) == -1){
+			// Rotação dupla a direita
+		}else{
+			// Rotação a direita
+		}
 	}
+	return x; 
 }
 
 int maximo(int esquerda, int direita){
@@ -64,11 +91,20 @@ int maximo(int esquerda, int direita){
 }
 
 int getAltura(No * arvore){
-	int esquerda, direita;
 	if (arvore == NULL)
-		return esquerda + 1; 
-	if(arvore.esquerda != NULL)
-		return esquerda += getAltura(arvore.esquerda);
-	if(arvore.direita != NULL)
-		return direita += getAltura(arvore.direita);
+		return -1; 
+	arvore.altura = maximo(getAltura(arvore.esquerda), getAltura(arvore.direita)) + 1;
+	return arvore.altura;
 }
+
+No busca(No & arvore, int chave){
+	if (arvore == NULL || chave == arvore.cliente.codigoCliente)
+		return arvore
+	if chave < arvore.cliente.codigoCliente
+		return busca(arvore.esquerda, chave);
+	else 
+		return busca(arvore.direita, chave);
+	
+}
+
+// Remoção de nó 
